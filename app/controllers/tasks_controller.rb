f0e79eee83c3c4a087b563e6beecfe4520ba8b68
taku.shuku <taku.shuku@outlook.jp>
2019-06-04 13:59:28 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+
   def index
     @tasks = Task.order(id: :desc).page(params[:page]).per(3)
   end
@@ -13,11 +15,10 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が登録されませんでした'
       render :new
@@ -30,7 +31,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -41,7 +42,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -51,8 +52,14 @@ class TasksController < ApplicationController
   end
 
 
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
