607d928e101de94971089bedbbaf4a0a49803085
taku.shuku <taku.shuku@outlook.jp>
2019-06-06 13:16:31 +0000

    tasklist.l14-1

--- db/migrate/20190514131226_create_tasks.rb
+++ db/migrate/20190514131226_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
-#  def change
-#    create_table :tasks do |t|
-#      t.string :content
+  def change
+    create_table :tasks do |t|
+      t.string :content
 
-#      t.timestamps
-#    end
-#  end
+      t.timestamps
+    end
+  end
 end
