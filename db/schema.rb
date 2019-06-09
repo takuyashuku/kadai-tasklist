2b436130cece4329eef4a97277619d79b516d52d
taku.shuku <taku.shuku@outlook.jp>
2019-06-09 14:43:56 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_140042) do
+ActiveRecord::Schema.define(version: 2019_06_09_141918) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_05_29_140042) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
