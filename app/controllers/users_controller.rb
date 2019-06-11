c84b28f98eaa51d7be9583964c8257a57de03023
taku.shuku <taku.shuku@outlook.jp>
2019-06-11 15:23:57 +0000

    fix users_controller.rb,new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
+
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
   
   def new
     @user = User.new
